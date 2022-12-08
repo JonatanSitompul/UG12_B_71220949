9085154a98c8f8e682120da6c9797012e6cfee65
JonatanSitompul <117884416+JonatanSitompul@users.noreply.github.com>
2022-12-08 12:51:42 +0700

    Add files via upload

---
+++ Soal_1.py
@@ -0,0 +1,8 @@
+print('Selamat datang di program pembuat piramida berlubang')
+b=int(input('Masukkan angka; '))
+
+ruang = a - 1
+for i in range(n):
+    print(' '* ruang, end= '')
+    print('*' if = 0 else '**' if i ! == 0 and  != n-1 '*'(n*2-1 ))
+    ruang -= 1
---
+++ Soal_2.py
@@ -0,0 +1,5 @@
+a=int(input('Masukkan Angka'))
+for i in range(1,10):
+    if i == 10:
+        continue
+    print(i, ' x ',i ,' = ',i*i)
